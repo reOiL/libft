5de5f975c501939529a2d9ba6b8e1eacc350c3b4
Johnny Webber <jwebber@ga-h2.21-school.ru>
2019-09-06 17:17:33 +0300

    added strdup

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: jwebber <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 16:59:14 by jwebber           #+#    #+#             */
-/*   Updated: 2019/09/05 18:21:04 by jwebber          ###   ########.fr       */
+/*   Updated: 2019/09/06 17:15:44 by jwebber          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,24 @@
 # define LIBFT_H
 
 # include <string.h>
+# include <stdlib.h>
 
 void	*ft_memset(void *b, int c, size_t len);
 
 void	ft_bzero(void *s, size_t n);
 
 void	*ft_memcpy(void *dst, const void *src, size_t n);
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+
+void	*ft_memmove(void *dst, const void *src, size_t len);
+
+void	*ft_memchr(const void *s, int c, size_t n);
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
+size_t	ft_strlen(const char *s);
+
+char	*ft_strdup(const char *src);
 
 #endif
---
+++ src/ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwebber <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/03 18:02:04 by jwebber           #+#    #+#             */
+/*   Updated: 2019/09/06 17:16:38 by jwebber          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+char	*ft_strdup(const char *src)
+{
+	char	*n_str;
+	int		i;
+
+	i = 0;
+	n_str = (char *)malloc(sizeof(char) * ft_strlen(src));
+	if (!n_str)
+		return (NULL);
+	while (src[i] != '\0')
+	{
+		n_str[i] = src[i];
+		i++;
+	}
+	n_str[i] = '\0';
+	return (n_str);
+}
