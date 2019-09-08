2b85e0373aaa3b2658abc1996d6fc98fefa1c2d1
Johnny Webber <jwebber@vo-f4.21-school.ru>
2019-09-08 09:02:52 +0300

    Update memalloca

--- src/ft_memalloc.c
+++ src/ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: jwebber <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/08 08:54:29 by jwebber           #+#    #+#             */
-/*   Updated: 2019/09/08 08:59:15 by jwebber          ###   ########.fr       */
+/*   Updated: 2019/09/08 09:02:32 by jwebber          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,10 @@
 void	*ft_memalloc(size_t size)
 {
 	void	*ptr;
-	size_t	i;
 
 	ptr = (void *)malloc(size);
 	if (ptr == NULL)
 		return (NULL);
-	i = 0;
-	while (i < size)
-	{
-		((unsigned char *)ptr)[i] = 0;
-		i++;
-	}
+	ft_memset(ptr, 0, size);
 	return (ptr);
 }
