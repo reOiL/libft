53fbf80a53c6f8f20cbc7eb9e5e36ca0571713ad
Johnny Webber <jwebber@vo-j5.21-school.ru>
2019-09-09 18:08:08 +0300

    Fix strtrim

--- src/ft_strtrim.c
+++ src/ft_strtrim.c
@@ -6,35 +6,68 @@
 /*   By: jwebber <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/08 10:14:37 by jwebber           #+#    #+#             */
-/*   Updated: 2019/09/08 10:55:48 by jwebber          ###   ########.fr       */
+/*   Updated: 2019/09/09 18:07:56 by jwebber          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-char	*ft_strtrim(char const *s)
+static int	ft_needtrim(char c)
+{
+	return (c == ' ' || c == '\n' || c == '\t');
+}
+
+static int	ft_trim_start(char const *s)
+{
+	int		a;
+
+	if (s == NULL)
+		return (0);
+	a = 0;
+	while (ft_needtrim(s[a]) && s[a] != '\0')
+		a++;
+	return (a);
+}
+
+static int	ft_trim_end(char const *s, size_t len)
+{
+	int	a;
+
+	if (s == NULL)
+		return (0);
+	a = 0;
+	while (len != 0 && ft_needtrim(s[len - 1]))
+	{
+		a++;
+		len--;
+	}
+	return (a);
+}
+
+char		*ft_strtrim(char const *s)
 {
 	size_t		len;
+	char		*ret;
 	size_t		start;
 	size_t		end;
-	char		*ret;
-	long long	new_len;
+	size_t		i;
 
 	if (s == NULL)
 		return (NULL);
-	start = 0;
 	len = ft_strlen(s);
-	while (s[start] == ' ' || s[start] == '\n' || s[start] == '\t')
-		start++;
-	end = 0;
-	while (s[(len - 1) - end] == ' ' ||
-			s[(len - 1) - end] == '\n' ||
-			s[(len - 1) - end] == '\t')
-		end++;
-	new_len = (len - (start + end)) + 1;
-	new_len = (new_len <= 0 ? 1 : new_len);
-	ret = ft_strnew(new_len);
-	if (ret == NULL)
+	start = ft_trim_start(s);
+	if (start != len)
+		end = ft_trim_end(s, len);
+	else
+		end = 0;
+	if (!(ret = ft_strnew(len - (start + end))))
 		return (NULL);
-	return (ft_strncpy(ret, (s + start), new_len - 1));
+	i = 0;
+	while (start < len - end)
+	{
+		ret[i] = s[start];
+		start++;
+		i++;
+	}
+	return (ret);
 }
