56a11fc86814b4617d34bf328085e7a1af2ec759
Johnny Webber <jwebber@vo-j5.21-school.ru>
2019-09-09 18:27:51 +0300

    Fix lstmap

--- src/ft_lstmap.c
+++ src/ft_lstmap.c
@@ -6,25 +6,20 @@
 /*   By: jwebber <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/08 13:51:58 by jwebber           #+#    #+#             */
-/*   Updated: 2019/09/08 14:11:19 by jwebber          ###   ########.fr       */
+/*   Updated: 2019/09/09 18:26:59 by jwebber          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
+#include <stdio.h>
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list	*ret;
 
-	if (lst)
-	{
-		ret = f(lst);
-		lst = lst->next;
-	}
-	while (lst)
-	{
-		ret = f(lst);
-		lst = lst->next;
-	}
+	if (!lst || !f)
+		return (NULL);
+	ret = f(lst);
+	ret->next = ft_lstmap(lst->next, f);
 	return (ret);
 }
