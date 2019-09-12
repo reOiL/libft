b84f3883193b1fc6cb363e2312fbb440c99004e4
Johnny Webber <jwebber@ga-h2.21-school.ru>
2019-09-12 19:28:16 +0300

    Fixed bug

--- src/ft_lstmap.c
+++ src/ft_lstmap.c
@@ -6,12 +6,11 @@
 /*   By: jwebber <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/08 13:51:58 by jwebber           #+#    #+#             */
-/*   Updated: 2019/09/12 17:13:37 by jwebber          ###   ########.fr       */
+/*   Updated: 2019/09/12 19:24:48 by jwebber          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
-#include <stdio.h>
+#include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
